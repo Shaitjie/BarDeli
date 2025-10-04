d618ad59ad21e5fd3edf92436dad27f44d97bbea
PatricNcube01 <patrictinashe.ncube@gmail.com>
2025-10-04 19:11:32 +0200

    Delete BarDeli-main directory

--- BarDeli-main/Supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// Your Supabase credentials
-const supabaseUrl = 'https://fawlfsukrqrucrrxrjvq.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhd2xmc3VrcnFydWNycnhyanZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0MzU2OTUsImV4cCI6MjA3MTAxMTY5NX0.KGH5SAJvCEhnz3NHdp6LeMgCvZp-nP1Tj6oIL32J6K8'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
--- BarDeli-main/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./**/*.html"], // <-- scan all HTML files for classes
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
